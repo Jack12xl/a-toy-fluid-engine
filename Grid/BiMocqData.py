0963b6d5546932c058bdf87fa1761e72b1bd089d
Jack12xl <jack12xieling@163.com>
2020-12-05 00:31:47 +0800

    SL bimocq

--- Grid/BiMocqData.py
+++ Grid/BiMocqData.py
@@ -12,3 +12,27 @@ class BimMocqGridData(MacGridData):
         super(BimMocqGridData, self).__init__(cfg)
 
         # 3.3 forward map
+        self.forward_map = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
+                                    dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
+
+        self.forward_map_bffr = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
+                                         dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
+
+        self.backward_map = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
+                                     dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
+
+        self.backward_map_bffr = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
+                                          dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
+
+    def materialize(self):
+        super(BimMocqGridData, self).materialize()
+        # init the forward and backward, t == 0 should map to itself
+        self.init_map(self.forward_map)
+        self.init_map(self.backward_map)
+        self.init_map(self.forward_map_bffr)
+        self.init_map(self.backward_map_bffr)
+
+    @ti.kernel
+    def init_map(self, m: Matrix):
+        for I in ti.static(m):
+            m[I] = m.getW(I)
