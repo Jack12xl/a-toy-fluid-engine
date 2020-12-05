35fa5a31f325ebf39930d4b34ec7b6cd03ef48e4
Jack12xl <jack12xieling@163.com>
2020-12-05 22:43:24 +0800

    SL for double advect, want to test offset of T and rho

--- Scheme/EulerScheme/BiMocq.py
+++ Scheme/EulerScheme/BiMocq.py
@@ -3,32 +3,44 @@ import taichi_glsl as ts
 import numpy as np
 from .Euler_Scheme import EulerScheme
 from config import VisualizeEnum, SceneEnum, SchemeType, SimulateType
-from utils import Vector, Matrix
+from utils import Vector, Matrix, Wrapper
 
 # ref Bimocq 2019, By qi ziyin
 err = 0.0001
 
+
 class Bimocq_Scheme(EulerScheme):
     def __init__(self, cfg):
         super().__init__(cfg)
+
+        self.blend_coefficient = 0.5
         pass
 
     def advect(self, dt):
         self.updateForward(self.grid.forward_map)
         self.updateForward(self.grid.forward_scalar_map)
+        self.updateBackward(self.grid.backward_map)
+        self.updateBackward(self.grid.backward_scalar_map)
+        # advect velocity, temperature, density
+        super(Bimocq_Scheme, self).advect(dt)
 
-        self.advect_velocity(dt)
+        # IntegrateMultiLevel {3.5}
+        self.grid.v_pair.nxt.fill(ts.vecND(self.dim, 0.0))
+        self.grid.p_pair.nxt.fill(ts.vecND(self.dim, 0.0))
+        self.grid.density_pair.nxt.fill(ts.vecND(self.dim, 0.0))
 
-        self.advection_solver.advect(self.grid.v_pair.cur, self.grid.density_pair.cur, self.grid.density_pair.nxt, dt)
+    def advectBimocq_velocity(self, vf: Wrapper):
+        pass
 
-        if self.cfg.SimType == SimulateType.Gas:
-            self.advection_solver.advect(self.grid.v_pair.cur,
-                                         self.grid.t_pair.cur,
-                                         self.grid.t_pair.nxt,
-                                         dt)
-            self.grid.t_pair.swap()
-        self.grid.swap_v()
-        self.grid.density_pair.swap()
+    @ti.kernel
+    def advect_twice_kern(self, f: Wrapper):
+        """
+        advect twice
+        :param f:
+        :return:
+        """
+
+        pass
 
     @ti.func
     def advectDMC(self, M, dt):
@@ -40,11 +52,28 @@ class Bimocq_Scheme(EulerScheme):
 
     @ti.func
     def solveODE_DMC(self, pos, dt):
+        """
+        Based on (12) ~ (14)
+        :param pos:
+        :param dt:
+        :return:
+        """
+        vf = ti.static(self.grid.v_pair.cur)
+
         a = self.calculateA(pos)
         # trace DMC
         # TODO DMC requires CFL < 1.0
+        vel = vf.interpolate(pos)
+        dmc_trace_pos = pos - dt * vel
 
-        pass
+        back_trace_pos = self.advection_solver.backtrace(vf, pos, dt)
+        for d in ti.static(self.dim):
+            if ti.abs(a[d]) > err:
+                dmc_trace_pos[d] = pos[d] - (1.0 - ti.exp(-a[d] * dt)) * vel[d] / a[d]
+            else:
+                dmc_trace_pos[d] = back_trace_pos[d]
+
+        return dmc_trace_pos
 
     @ti.func
     def calculateA(self, pos):
@@ -54,20 +83,21 @@ class Bimocq_Scheme(EulerScheme):
         :return:
         """
         vf = ti.static(self.grid.v_pair.cur)
-        vel = vf[vf.getG(pos)]
+
+        vel = vf.interpolate(pos)
 
         new_pos = pos + vf.dx * ts.sign(vel)
-        new_vel = vf[vf.getG(new_pos)]
+        new_vel = vf.interpolate(new_pos)
 
         return (new_vel - vel) / (new_pos - pos + err)
 
-
     @ti.kernel
     def updateBackward(self, M: Matrix, dt: ti.f32):
+        tmp_M = ti.static(self.grid.tmp_map)
         # DMC
         self.advectDMC(M, dt)
-        # self.advection_solver.advect(vf, M, tmp_M, dt)
-        pass
+
+        M, tmp_M = tmp_M, M
 
     @ti.kernel
     def updateForward(self, M: Matrix, dt: ti.f32):
@@ -80,10 +110,12 @@ class Bimocq_Scheme(EulerScheme):
         vf = ti.static(self.grid.v_pair.cur)
         for I in ti.static(M):
             pos = M[I]
+            # TODO maybe need clamp here
             M[I] = self.advection_solver.backtrace(vf, pos, -dt)
 
     def schemeStep(self, ext_input: np.array):
         self.advect(self.cfg.dt)
+        self.externalForce(ext_input, self.cfg.dt)
 
         self.project()
         self.grid.subtract_gradient(self.grid.v_pair.cur, self.grid.p_pair.cur)
---
+++ test/test_approx.py
@@ -0,0 +1,19 @@
+import taichi as ti
+import taichi_glsl as ts
+
+ti.init(ti.gpu)
+
+
+@ti.func
+def t(a):
+    return a == ti.approx(ts.vec3(233.0))
+
+
+@ti.kernel
+def test_approx():
+    a = ts.vec3(233.0)
+    print(t(a))
+
+
+if __name__ == "__main__":
+    test_approx()
