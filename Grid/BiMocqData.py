46820f74b7cdb1810b62cd2c0cd72bf2b90df760
Jack12xl <jack12xieling@163.com>
2020-12-05 15:35:03 +0800

    SL for forward

--- Grid/BiMocqData.py
+++ Grid/BiMocqData.py
@@ -12,25 +12,25 @@ class BimMocqGridData(MacGridData):
         super(BimMocqGridData, self).__init__(cfg)
 
         # 3.3 forward map
-        self.forward_map = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
-                                    dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
 
-        self.forward_map_bffr = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
-                                         dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
-
-        self.backward_map = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
-                                     dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
-
-        self.backward_map_bffr = CellGrid(ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
-                                          dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0))
+        self.forward_map, self.backward_map, self.backward_map_bffr, \
+        self.forward_scalar_map, self.backward_scalar_map, self.backward_scalar_map_bffr = \
+            [CellGrid(
+                ti.Vector.field(cfg.dim, dtype=ti.f32, shape=cfg.res), cfg.dim,
+                dx=ts.vecND(self.dim, self.cfg.dx), o=ts.vecND(self.dim, 0.0)
+            )
+                for _ in range(6)]
 
     def materialize(self):
         super(BimMocqGridData, self).materialize()
         # init the forward and backward, t == 0 should map to itself
         self.init_map(self.forward_map)
+        self.init_map(self.forward_scalar_map)
+
         self.init_map(self.backward_map)
-        self.init_map(self.forward_map_bffr)
         self.init_map(self.backward_map_bffr)
+        self.init_map(self.backward_scalar_map)
+        self.init_map(self.backward_scalar_map_bffr)
 
     @ti.kernel
     def init_map(self, m: Matrix):
--- Scheme/EulerScheme/BiMocq.py
+++ Scheme/EulerScheme/BiMocq.py
@@ -3,7 +3,7 @@ import taichi_glsl as ts
 import numpy as np
 from .Euler_Scheme import EulerScheme
 from config import VisualizeEnum, SceneEnum, SchemeType, SimulateType
-
+from utils import Vector, Matrix
 
 # ref Bimocq 2019, By qi ziyin
 
@@ -13,6 +13,9 @@ class Bimocq_Scheme(EulerScheme):
         pass
 
     def advect(self, dt):
+        self.updateForward(self.grid.forward_map)
+        self.updateForward(self.grid.forward_scalar_map)
+
         self.advect_velocity(dt)
 
         self.advection_solver.advect(self.grid.v_pair.cur, self.grid.density_pair.cur, self.grid.density_pair.nxt, dt)
@@ -32,13 +35,19 @@ class Bimocq_Scheme(EulerScheme):
     def updateBackward(self, dt):
         pass
 
-    def updateForward(self, dt):
-        pass
+    def updateForward(self, M: Matrix, dt: ti.f32):
+        """
+
+        :param M: wrapper for mapper
+        :param dt:
+        :return:
+        """
+        vf = ti.static(self.grid.v_pair.cur)
+        for I in ti.static(M):
+            pos = M[I]
+            M[I] = self.advection_solver.backtrace(vf, pos, dt)
 
     def schemeStep(self, ext_input: np.array):
-        self.updateForward(self.cfg.dt)
-        self.updateBackward(self.cfg.dt)
-
         self.advect(self.cfg.dt)
 
         self.project()
