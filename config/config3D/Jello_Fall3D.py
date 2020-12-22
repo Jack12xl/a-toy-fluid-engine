9ecf8e473a175cfd23a66ad5cadb5ef1005d184f
Jack12 <jack12xieling@163.com>
2020-12-23 03:40:27 +0800

    add more particle

--- config/config3D/Jello_Fall3D.py
+++ config/config3D/Jello_Fall3D.py
@@ -10,6 +10,7 @@ layout_method = DLYmethod.AoS
 quality = 1
 
 dt = 1e-4 / quality
+n_particle = 18000 * quality ** 2
 
 n_grid = 128 * quality
 dx = 1.0 / n_grid
@@ -25,5 +26,5 @@ E, nu = 1e6, 0.2
 
 ti.init(arch=ti.gpu, debug=False, kernel_profiler=True)
 
-profile_name = "MPM{}D-Q-{}-dt-{}".format(dim, quality, dt)
+profile_name = "MPM{}D-P-{}-G-{}-dt-{}".format(dim, n_particle, 'x'.join(map(str, res)), dt)
 bool_save = False
